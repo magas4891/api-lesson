873c942fbffb4458b033e9ca5d947b7f04f7629c
magas4891 <magasishe@gmail.com>
2020-01-03 17:30:50 +0200

    primitive styles

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :main, path: '', only: [:create]
   resources :excavators
   resources :tickets
+  root 'tickets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
